7073857ace40b01b38524eefc83d5c08bcac834c
feras alnowiser <ferasdev@gmail.com>
2022-08-30 21:25:47 +0300

    adding the cellClicked function selecting X or O or red flash for over typing and showing game finished if all cells are selected

--- main.js
+++ main.js
@@ -11,5 +11,37 @@ $(document).ready(function () {
     const winner = $('#winner');
     let playerWin = $('#player')
     const restart = $('#restart')
+    let counter = 0
+    const X = 'X'
+    const O = 'O'
+
+    function cellClicked() {
+    
+        if($(this).text() === '' && counter % 2 == 0 && counter <9) {
+            $(this).text(X)
+            turn.text(O)
+            counter++;
+        } 
+        else if($(this).text() === '' && counter % 2 !== 0 && counter <9) {
+            $(this).text(O)
+            turn.text(X)
+            counter++;
+            
+
+        } else {
+            const currentThis = $(this)
+            $(this).css('background-color', '#ffcccb')
+            setTimeout(function(){
+                currentThis.css('background-color', '#E2EEF3')
+           },250); // for 0.25s = 250ms
+        }
+
+        if(counter >= 9) {
+            winner.text('Game Finished');
+            return;
+        }
+    }
+
+    cells.click(cellClicked)
 
 })
